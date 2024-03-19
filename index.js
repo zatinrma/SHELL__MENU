43f005e5bfae9d59fa174378b3769367e4ec9639
zatinrma <ymazatin@gmail.com>
2024-03-20 01:30:10 +0700

    [IMP] Restructure code

--- index.js
+++ index.js
@@ -22,13 +22,31 @@ function show_hide_immediate_change_menu() {
     }
 }
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
+    // Auto Change the Image with White Light Fade In Out 
+    let slideIndex = 0;
+    showSlides();
+
+    function showSlides() {
+        let i;
+        let slides = document.getElementsByClassName("mySlides");
+        let dots = document.getElementsByClassName("dot");
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) { slideIndex = 1 }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace("active", "");
+        }
+        slides[slideIndex - 1].style.display = "block";
+        dots[slideIndex - 1].className += " active";
+        setTimeout(showSlides, 8000); // Change image every 2 seconds
+    }
+
     // Code inside this function will run after the DOM has been fully loaded
     let image_1 = document.getElementById('slide_img_1');
     let image_2 = document.getElementById('slide_img_2');
-    
-    document.getElementById("ASDF").innerHTML = "Hello World";
-    
     let images = [
         './Image/shell (2).jpg',
         './Image/shell (3).jpg',
@@ -45,7 +63,6 @@ document.addEventListener("DOMContentLoaded", function() {
         './Image/shell (14).jpg',
     ]
     function random_interval() {
-        console.log('BBBBBBBBBBBBBBB')
         let random_1 = Math.floor(Math.random() * 12);  // Java Script List has quantity 13 file starting in list from 0 - 12
         let random_2 = random_1
         if (random_1 == 13) {
@@ -54,12 +71,9 @@ document.addEventListener("DOMContentLoaded", function() {
         else {
             random_2 = random_1 + 1
         }
-        console.log('CCCCCCCCCCCCCCCCC', images[random_1], typeof(images[random_1]))
         image_1.src = images[random_1];
         image_2.src = images[random_2];
     }
-    setInterval(random_interval, 3000)    
-    // Access the image element and set its src attribute
-    
+    setInterval(random_interval, 3000)
 });
 
